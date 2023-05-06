7abda05b4b0557beb144a1cd4f863a2816cfac56
EvilPrincess <defdXs@yandex.ru>
2023-05-07 00:11:00 +0300

    Add files via upload

---
+++ CodeSamples/Classes/resources.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <windowsx.h>
+#include <windows.h>
+#include <winuser.h>
+#include <tchar.h>
+#include <string>
+#include <vector>
+#include <map>
+#include <fstream>
+
+using namespace std;
