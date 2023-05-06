04b449979e5ae466595ee574b81599e5e35350db
EvilPrincess <defdXs@yandex.ru>
2023-05-07 00:08:52 +0300

    Update Vector3.h

--- CodeSamples/Classes/Vector3/Vector3.h
+++ CodeSamples/Classes/Vector3/Vector3.h
@@ -1,1 +1,47 @@
+#pragma once
+#include <windows.h>
+#include <string>
+class Vector3
+{
+private:
+	LONG length;
+public:
+	LONG x, y, z;
 
+	Vector3();
+	Vector3(long _x, long _y, long _z);
+	DOUBLE Len();
+	Vector3 Normalized();
+	DOUBLE cos(Vector3 _v3);
+	void Invalidate();
+
+	Vector3 operator += (const Vector3& _Other);
+	Vector3 operator += (const DOUBLE& _Other);
+	Vector3 operator -= (const Vector3& _Other);
+	Vector3 operator -= (const DOUBLE& _Other);
+	Vector3 operator *= (const DOUBLE& _Other);
+	Vector3 operator /= (const DOUBLE& _Other);
+	Vector3 operator = (const Vector3& _Other);
+
+	friend Vector3 operator + (const Vector3& _Left, const Vector3& _Right);
+	friend Vector3 operator + (const Vector3& _Left, const DOUBLE& _Right);
+	friend Vector3 operator - (const Vector3& _Left, const Vector3& _Right);
+	friend Vector3 operator - (const Vector3& _Left, const DOUBLE& _Right);
+	friend DOUBLE operator * (const Vector3& _Left, const Vector3& _Right);
+	friend Vector3 operator * (const Vector3& _Left, const DOUBLE& _Right);
+	friend Vector3 operator / (const Vector3& _Left, const DOUBLE& _Right);
+	friend BOOL operator == (const Vector3& _Left, const Vector3& _Right);
+
+	static const Vector3 ZERO;
+	static const Vector3 RIGHT;
+	static const Vector3 LEFT;
+	static const Vector3 FORTH;
+	static const Vector3 BACK;
+	static const Vector3 UP;
+	static const Vector3 DOWN;
+};
+
+typedef Vector3 V3;
+typedef Vector3 Vec3;
+typedef Vector3 VEC3;
+typedef Vector3 VECTOR3;
