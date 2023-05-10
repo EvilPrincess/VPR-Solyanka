cffa97c07d58b14cc21199aa9bfeb88c84f35555
EvilPrincess <defdXs@yandex.ru>
2023-05-10 23:39:54 +0300

    Update LinkedStack.cpp

--- CodeSamples/Structs/LinkedStack.cpp
+++ CodeSamples/Structs/LinkedStack.cpp
@@ -6,7 +6,7 @@ struct LinkedStack
 {
 	typedef unsigned int UINT;
 
-// Элемент стека
+	// Элемент стека
 	struct Node
 	{
 		T value = T{};
@@ -14,11 +14,16 @@ struct LinkedStack
 
 		Node(T value, Node* next = nullptr) : value(value), next(next) { };
 	};
-// Указатель на вершину стека
+	// Указатель на вершину стека
 	Node* head = nullptr;
 
-// Возвращает _Idx-й элемент в стеке
-	Node* Get(UINT _Idx)
+	// Возвращает значение _Idx-того элемента в стеке
+	T Get(UINT _Idx)
+	{
+		return GetNode(_Idx)->value;
+	}
+	// Возвращает _Idx-й элемент в стеке
+	Node* GetNode(UINT _Idx)
 	{
 		UINT depth = 0;
 		Node* node = head;
@@ -30,12 +35,12 @@ struct LinkedStack
 		}
 		return node;
 	}
-// Индексация, чтоб вместо Get
-	Node* operator [] (UINT _Idx)
+	// Индексация, чтоб вместо Get
+	T operator [] (UINT _Idx)
 	{
 		return Get(_Idx);
 	}
-// Добавляет в начало новый элемент, возвращает указатель на него
+	// Добавляет в начало новый элемент, возвращает указатель на него
 	Node* push(T _Val)
 	{
 		if (!head)
@@ -46,7 +51,7 @@ struct LinkedStack
 		head = new Node(_Val, head);
 		return head;
 	}
-// Удаляет первый элемент в стеке (0-й), возвращает его копию
+	// Удаляет первый элемент в стеке (0-й), возвращает его копию
 	const Node pop()
 	{
 		if (head)
@@ -59,8 +64,8 @@ struct LinkedStack
 		}
 		else throw std::out_of_range("fix ur life idiot");
 	}
-// Возвращает индекс первого найденного элемента в стеке
-// со значением _Val
+	// Возвращает индекс первого найденного элемента в стеке
+	// со значением _Val
 	const int find(T _Val)
 	{
 		UINT depth = 0;
@@ -72,7 +77,7 @@ struct LinkedStack
 			else { node = node->next; depth++; }
 		}
 	}
-// Возвращает размер (кол-во эдементов) стека
+	// Возвращает размер (кол-во эдементов) стека
 	const UINT size()
 	{
 		UINT depth = 0;
@@ -80,26 +85,48 @@ struct LinkedStack
 		while (node) { node = node->next; depth++; }
 		return depth;
 	}
-// Выводит в консоль через ostream весь стек, як список в питоне
-	void CmdOut(void)
+	// Выводит в консоль через ostream весь стек, як список в питоне
+	void CmdOut(bool doEndl = 1, std::ostream& os = std::cout)
 	{
+		if (size() == 0)
+		{
+			os << "[ NIL ]" << (doEndl? "\n" : "");
+			return;
+		}
 		Node* node = head;
-		std::cout << "[ ";
+		os << "[ ";
 		while (node)
 		{
-			std::cout << node->value << (node->next? ", " : " ");
+			os << node->value << (node->next ? ", " : " ");
 			node = node->next;
 		}
-		std::cout << "]" << std::endl;
+		os << "]" << (doEndl ? "\n" : "");
 	}
-// Меняет местами два элемента в стеке и индексами _a и _b
+	friend ostream& operator << (ostream& os, LinkedStack& ls)
+	{
+		ls.CmdOut(1, os);
+		return os;
+	}
+	friend ostream& operator << (ostream& os, LinkedStack* ls)
+	{
+		ls->CmdOut(1, os);
+		return os;
+	}
+	// Меняет местами два элемента в стеке и индексами _a и _b
 	void swap(UINT _a, UINT _b)
-	{	
+	{
+		if (_a == _b) return;
+		if (_a > _b)
+		{
+			UINT tmp = _a;
+			_a = _b;
+			_b = tmp;
+		}
 		if (_b - _a > 1)
 		{
 			if (_a == 0)
 			{
-				Node* np2 = Get(_b - 1);
+				Node* np2 = GetNode(_b - 1);
 				Node* n2 = np2->next;
 				Node* nn2 = n2->next;
 
@@ -110,10 +137,10 @@ struct LinkedStack
 			}
 			else
 			{
-				Node* np1 = Get(_a - 1);
+				Node* np1 = GetNode(_a - 1);
 				Node* n1 = np1->next;
 				Node* nn1 = n1->next;
-				Node* np2 = Get(_b - 1);
+				Node* np2 = GetNode(_b - 1);
 				Node* n2 = np2->next;
 				Node* nn2 = n2->next;
 				np1->next = n2;
@@ -133,9 +160,9 @@ struct LinkedStack
 			}
 			else
 			{
-				Node* np1 = Get(_a - 1);
+				Node* np1 = GetNode(_a - 1);
 				Node* n1 = np1->next;
-				Node* n2 = Get(_b);
+				Node* n2 = GetNode(_b);
 				Node* nn2 = n2->next;
 				np1->next = n2;
 				n2->next = n1;
@@ -144,16 +171,17 @@ struct LinkedStack
 		}
 		else throw;
 	}
-// Меняет направление элементов
-	void reverse(void)
+	// Меняет направление элементов в стеке
+	LinkedStack* reverse(void)
 	{
 		size_t sz = size();
 		for (size_t i = 0; i < sz / 2; i++)
 			swap(i, sz - i - 1);
+		return this;
 	}
-// Простое "a = b" вызовет полное копирование, т.е. по факту a и b будут указывать на одни и те же значения,
-// copy(куда?) вызовет заполнение "куда?" элементами с теми же значениями, что и в исходном стеке. 
-// Главное чтобы типы совпадали. После заполнения необходимо развернуть стек, если вам не нужно - copy(куда?, 0);
+	// Простое "a = b" вызовет полное копирование, т.е. по факту a и b будут указывать на одни и те же значения,
+	// copy(куда?) вызовет заполнение "куда?" элементами с теми же значениями, что и в исходном стеке. 
+	// Главное чтобы типы совпадали. После заполнения необходимо развернуть стек, если вам не нужно - copy(куда?, 0);
 	void copy(LinkedStack<T>& _Dest, bool doReverse = 1)
 	{
 		Node* node = head;
